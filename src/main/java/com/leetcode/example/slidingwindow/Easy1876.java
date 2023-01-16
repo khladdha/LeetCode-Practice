7e42fbc6b20dcbd0225f55458f54c82312be33ef
KalpeshL <kalpesh.laddha@capgemini.com>
2023-01-16 10:39:36 -0500

    Sliding Window Problem No 1876

---
+++ src/main/java/com/leetcode/example/slidingwindow/Easy1876.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.leetcode.example.slidingwindow;
+
+/**
+ * @author kladdha
+ *
+ */
+public class Easy1876 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
