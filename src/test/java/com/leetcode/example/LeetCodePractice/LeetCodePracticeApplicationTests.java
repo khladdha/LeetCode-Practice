05aaf533b97a58da5f64d9d62f1c538907bccb06
Laddha <kalpesh.laddha@capgemini.com>
2023-01-16 10:12:19 -0500

    First Commit
    Pushing complete project to github

---
+++ src/test/java/com/leetcode/example/LeetCodePractice/LeetCodePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leetcode.example.LeetCodePractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetCodePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
